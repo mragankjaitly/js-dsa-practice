222ea84c2d460ae3702afdbf6561768bc321ffe4
Mragank Jaitly <84132608+mragankjaitly@users.noreply.github.com>
2026-08-08 18:52:15 +0530

    Add files via upload

---
+++ Day_3.js
@@ -0,0 +1,12 @@
+// maxsubarray
+
+let maxSubArray = function (nums){
+    let currSum = nums[0];
+    let maxSum = nums[0];
+
+    for(let i = 1; i<nums.length; i++){
+        currSum = Math.max(nums[i], nums[i] + currSum);
+        maxSum = Math.max(maxSum, currSum);
+    }
+    return maxSum;
+}
